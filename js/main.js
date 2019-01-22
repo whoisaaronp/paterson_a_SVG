1b1c6c5d67975435f78d81780d883ffb0bba1e2f
Aaron Paterson <juniha.aaron@gmail.com>
2019-01-22 14:44:12 -0500

    its firing on click

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@
 (() => {
 
 // let invoke a fire!! to see if it's working?
-	console.log('Tis Hot in Here!!');
+	console.log('Tis firing!!');
 
 	// establish your variable naming convention.
-	var allMyicons = document.querySelectorAll(".svgIcons");
+	var allMyicons = document.querySelectorAll("img");
 	// debugger;
 	function logIconId(){
 		console.log(this.Id);
 
 	}
+
+
 	// we are listening for a click event for all the icons.
 	// next what happens to each icons when a click event is executed.
 	// allMyicons.addEventListener("click", logIconId);
